fb1e0a013d0d4450930ecc5b756ba49184bfdf8b
Rangaswamy vikas <vikas1029300@gmail.com>
2022-05-28 06:16:25 +0530

    Final project

--- Main/model.py
+++ Main/model.py
@@ -2,6 +2,10 @@
 from replace import *
 
 
+# Decision tree is classified as a very effective and versatile classification technique. It is used in pattern recognition and classification for image. It is used for classification in very complex problems dew to its high adaptability. It is also capable of engaging problems of higher dimensionality. It mainly consists of three parts root, nodes and leaf.
+# Roots consists of attribute which has most effect on the outcome, leaf tests for value of certain attribute and leaf gives out the output of tree.
+
+
 def DecisionTree():
 
     from sklearn import tree
@@ -38,7 +42,27 @@ def DecisionTree():
     else:
         t1.delete("1.0", END)
         t1.insert(END, "Not Found")
+    import sqlite3 
+    conn = sqlite3.connect('database.db') 
+    c = conn.cursor() 
+    c.execute("CREATE TABLE IF NOT EXISTS DecisionTree(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+    c.execute("INSERT INTO DecisionTree(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),t1.get("1.0",END)))
+    conn.commit()  
+    c.close() 
+    conn.close()
 
+
+# Random Forest Algorithm is a supervised learning algorithm used for both classification and regression. This algorithm works on 4 basic steps
+
+# 1. It chooses random data samples from dataset.
+
+# 2. It constructs decision trees for every sample dataset chosen.
+
+# 3. At this step every predicted result will be compiled and voted on.
+
+# 4. At last most voted prediction will be selected and be presented as result of classification.
+
+# In this project we have used random forest classifier with 100 random samples and the result given is -95 % accuracy.
 
 def randomforest():
     from sklearn.ensemble import RandomForestClassifier
@@ -75,8 +99,21 @@ def randomforest():
     else:
         t2.delete("1.0", END)
         t2.insert(END, "Not Found")
+        
+    import sqlite3
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS RandomForest(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+    c.execute("INSERT INTO RandomForest(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",
+              (NameEn.get(), Symptom1.get(), Symptom2.get(), Symptom3.get(), Symptom4.get(), Symptom5.get(), t2.get("1.0",END)))
+    conn.commit()
+    c.close()
+    conn.close()
 
 
+# Naïve Bayes algorithm is a family of algorithms based on naïve bayes theorem. They share a common principle that is every pair of prediction is independent of each other. It also makes an assumption that features make an independent and equal contribution to the prediction.
+
+# In our project we have used naïve bayes algorithm to gain a - 95 % accurate prediction.
 def NaiveBayes():
     from sklearn.naive_bayes import GaussianNB
     gnb = GaussianNB()
@@ -111,8 +148,26 @@ def NaiveBayes():
         t3.delete("1.0", END)
         t3.insert(END, "Not Found")
         
+    import sqlite3
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS NaiveBayes(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+    c.execute("INSERT INTO NaiveBayes(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",
+              (NameEn.get(), Symptom1.get(), Symptom2.get(), Symptom3.get(), Symptom4.get(), Symptom5.get(), t3.get("1.0", END)))
+    conn.commit()
+    c.close()
+    conn.close()
+
+    
+    
         
-def KNN():
+#  K Nearest Neighbour is a supervised learning algorithm. It is a basic yet essential algorithm. It finds extensive use in pattern finding and data mining.
+
+# It works by finding a pattern in data which links data to results and it improves upon the patter recognition with every iteration.
+
+# We have used K Nearest Neighbour to classify our dataset and achieved -92% accuracy.   
+    
+def Knn():
     from sklearn.neighbors import KNeighborsClassifier
     knn = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
     knn = knn.fit(X, np.ravel(y))
@@ -151,10 +206,29 @@ def KNN():
     else:
         t4.delete("1.0", END)
         t4.insert(END, "Not Found")
+        
+    import sqlite3
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS KNearestNeighbour(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+    c.execute("INSERT INTO KNearestNeighbour(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",
+              (NameEn.get(), Symptom1.get(), Symptom2.get(), Symptom3.get(), Symptom4.get(), Symptom5.get(), t4.get("1.0",END)))
+    conn.commit()
+    c.close()
+    conn.close()
          
-        
-
-        
+ 
+def predictDisease(symptoms):
+    
+    
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": nb_prediction,
+        "final_prediction": final_prediction
+}
+    return predictions
 
 
 # GUI stuff..............................................................................
@@ -183,14 +257,11 @@ w2 = Label(root, justify=LEFT,
            text="Dr.ONLINE", fg="red", bg="White")
 w2.config(font=("Times", 30, "bold italic"))
 w2.grid(row=1, column=0, columnspan=2, padx=100)
-# w2 = Label(root, justify=LEFT, text="",
-#            fg="Pink", bg="Blue")
-# w2.config(font=("Times", 30, "bold italic"))
-# w2.grid(row=2, column=0, columnspan=2, padx=100)
 
-# NameLb = Label(root, text="Name of the Patient", fg="Red", bg="Sky Blue")
-# NameLb.config(font=("Times", 15, "bold italic"))
-# NameLb.grid(row=6, column=0, pady=15, sticky=W)
+
+NameLb = Label(root, text="Name of the Patient", fg="Red", bg="Sky Blue")
+NameLb.config(font=("Times", 15, "bold italic"))
+NameLb.grid(row=6, column=0, pady=15, sticky=W)
 
 S1Lb = Label(root, text="Symptom 1", fg="Blue", bg="Pink")
 S1Lb.config(font=("Times", 15, "bold italic"))
@@ -231,8 +302,8 @@ ranfLb.grid(row=21, column=0, pady=10, sticky=W)
 
 OPTIONS = sorted(l1)
 
-# NameEn = Entry(root, textvariable=Name)
-# NameEn.grid(row=6, column=1)
+NameEn = Entry(root, textvariable=Name)
+NameEn.grid(row=6, column=1)
 
 S1 = OptionMenu(root, Symptom1, *OPTIONS)
 S1.grid(row=7, column=1)
@@ -266,7 +337,7 @@ lr.config(font=("Times", 15, "bold italic"))
 lr.grid(row=10, column=3, padx=10)
 
 lr = Button(root, text="Prediction 4",
-            command=KNN, bg="Blue", fg="white")
+            command= Knn, bg="yellow", fg="blue")
 lr.config(font=("Times", 15, "bold italic"))
 lr.grid(row=11, column=3, padx=10)
 
@@ -283,8 +354,11 @@ t3 = Text(root, height=1, width=40, bg="red", fg="white")
 t3.config(font=("Times", 15, "bold italic"))
 t3.grid(row=19, column=1, padx=10)
 
-t4 = Text(root, height=1, width=40, bg="red", fg="white")
+t4 = Text(root, height=1, width=40, bg="purple", fg="white")
 t4.config(font=("Times", 15, "bold italic"))
 t4.grid(row=21, column=1, padx=10)
+
+
+
 
 root.mainloop()
